b9517f374cbdabcd976a0698912519d0a6831e46
SohagTech <mislamsohag@gmail.com>
2023-09-27 23:41:22 +0600

    Task_3 Update

--- task_3.php
+++ task_3.php
@@ -1,18 +1,31 @@
-
 <?php
 /* 
 Task 3: Array Sorting  
 
 Create an array called $grades with the following values: 85, 92, 78, 88, 95. Write a PHP function which takes "$grades" as an argument to sort the array in descending order and print the sorted grades as array.
-
 */
 
+
+$grades=[85, 92, 78, 88, 95];
+
+function gradesChecker($grades){
+    rsort($grades);
+    return $grades;    
+}
+
+$array=gradesChecker($grades);
+print_r($array);
+
+
+// =========== or ==========
+
+/*
 $grades=[85, 92, 78, 88, 95];
 rsort($grades);
-// print_r($grades);
+print_r($grades);
 
-function gradeChecker($grades){
-    // echo $grades."\n";
+function gradeChecker($grades){   
+
     $newArray=[];
     if($grades>=80){
         $newArray["$grades"]='A+';
@@ -21,6 +34,7 @@ function gradeChecker($grades){
     }
     return $newArray;
 }
-
+ 
 $array=array_map("gradeChecker", $grades);
 print_r($array);
+*/
