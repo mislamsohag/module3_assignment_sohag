9ba7c6ddb7b98eaa7ddc63309badfe1b5a808861
SohagTech <mislamsohag@gmail.com>
2023-09-27 23:46:42 +0600

    Task_2 Update

--- task_2.php
+++ task_2.php
@@ -9,10 +9,10 @@ Create an array called $numbers containing the numbers 1 to 10. Write a PHP func
 
 $numbers = range(1, 10);
 
-function removeEvenNumbers(&$arr) {
-    foreach ($arr as $key => $number) {
+function removeEvenNumbers(&$array) {
+    foreach ($array as $key => $number) {
         if ($number % 2 === 0) {
-            unset($arr[$key]);
+            unset($array[$key]);
         }
     }
 }
